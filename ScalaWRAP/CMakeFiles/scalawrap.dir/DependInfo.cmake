4ba4d4161c7b835a58c62a51853cf02cc0bdaee4
Jessie Lauzon <jessicatlauzon@gmail.com>
2019-09-25 14:44:15 -0700

    add ScalaWRAP files

---
+++ ScalaWRAP/CMakeFiles/scalawrap.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/src/BLAS.cpp" "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/CMakeFiles/scalawrap.dir/src/BLAS.cpp.o"
+  "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/src/C_interface.cpp" "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/CMakeFiles/scalawrap.dir/src/C_interface.cpp.o"
+  "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/src/Context.cpp" "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/CMakeFiles/scalawrap.dir/src/Context.cpp.o"
+  "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/src/MPI_utils.cpp" "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/CMakeFiles/scalawrap.dir/src/MPI_utils.cpp.o"
+  "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/src/SVD.cpp" "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/CMakeFiles/scalawrap.dir/src/SVD.cpp.o"
+  "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/src/ScalaMat.cpp" "/g/g20/lauzon1/Software/libROM-param/ScalaWRAP/CMakeFiles/scalawrap.dir/src/ScalaMat.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Intel")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_MKL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/tce/packages/mvapich2/mvapich2-2.3-intel-19.0.4/include"
+  "ScalaWRAP/src"
+  "/usr/tce/packages/mkl/mkl-2019.0/compilers_and_libraries/linux/mkl/include"
+  "ScalaWRAP"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ScalaWRAP/CMakeFiles/scalawrap.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scalawrap.dir/src/Context.cpp.o"
+  "CMakeFiles/scalawrap.dir/src/ScalaMat.cpp.o"
+  "CMakeFiles/scalawrap.dir/src/MPI_utils.cpp.o"
+  "CMakeFiles/scalawrap.dir/src/C_interface.cpp.o"
+  "CMakeFiles/scalawrap.dir/src/BLAS.cpp.o"
+  "CMakeFiles/scalawrap.dir/src/SVD.cpp.o"
+  "libscalawrap.pdb"
+  "libscalawrap.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scalawrap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
